aa74944b604c5bf128b1a676c8280fced8578c80
pascal.mekinda <pascal.mekinda@boostcompagny.com>
2023-11-11 21:44:31 +0100

    fin de cette version du code

--- app.js
+++ app.js
@@ -16,6 +16,54 @@ app.get("/message", (req, res) => {
   res.json({ message: "Ceci est un message de votre API Node.js du chatbot." });
 });
 
+// Créez des routes pour gérer les conversations avec le chatbot
+// app.post("/webhook", (req, res) => {
+//   if (req.body && req.body.message) {
+//     const userMessage = req.body.message;
+//     console.log("userMessage: ", userMessage);
+
+//     // Lisez le fichier JSON de réponses
+//     const responsesJson = fs.readFileSync("responses.json", "utf8");
+//     const responses = JSON.parse(responsesJson);
+//     // console.log("responses: ", responses);
+
+//     // Recherchez la question dans les réponses
+//     let botResponse = "Réponse du chatbot par défaut";
+
+//     //Ancienne methode
+//     //   for (const response of responses.responses) {
+//     //     if (userMessage.toLowerCase() === response.question.toLowerCase()) {
+//     //       botResponse = response.answer;
+//     //       break;
+//     //     }
+//     //   }
+
+//     // Nouvelle méthode
+//     for (const response of responses.responses) {
+//       if (response && response.question) {
+//         // Transformez les questions en minuscules et enlevez les espaces
+//         const userQuestion = userMessage.toLowerCase().replace(/\s/g, "");
+//         const responseQuestion = response.question
+//           .toLowerCase()
+//           .replace(/\s/g, "");
+
+//         // Vérifiez si la question de l'utilisateur contient la question de réponse
+//         if (userQuestion.includes(responseQuestion)) {
+//           botResponse = response.answer;
+//           break;
+//         }
+//       }
+//     }
+
+//     res.json({ message: botResponse });
+//   } else {
+//     res.status(400).json({
+//       error: "Le champ 'message' est manquant dans le corps de la requête.",
+//     });
+//   }
+// });
+
+// Nouvelle méthode avec NLP
 app.post("/webhook", (req, res) => {
   if (req.body && req.body.message) {
     const userMessage = req.body.message;
